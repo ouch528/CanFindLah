65f8a833a9847796b95edc68501e1b8361caf1a3
Xavier Hua <e1186218@u.nus.edu>
2025-03-15 14:20:56 +0800

    Sprint 1: Backlog 15: Changed and hide firebase credentials

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCk9coIXneZ3QnaHexlUBc-Op1s2-ZUJ6Q",
-    authDomain: "canfindlah-test.firebaseapp.com",
-    projectId: "canfindlah-test",
-    storageBucket: "canfindlah-test.firebasestorage.app",
-    messagingSenderId: "23605192075",
-    appId: "1:23605192075:web:e223878baab9c409847b8e",
-    measurementId: "G-XTS62G4PD7"
-};
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+}
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
